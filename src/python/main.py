f75d13c247fa97beb0df70d8be77864a0024a292
Hamid Aziz Protik <protikhamidaziz@gmail.com>
2026-02-02 11:45:42 +0600

    Update main.py

--- src/python/main.py
+++ src/python/main.py
@@ -1,6 +1,5 @@
 import os
 import pandas as pd
-import string
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
